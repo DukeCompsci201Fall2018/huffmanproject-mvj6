17b07a0051301f42a75ce189c0ffa8d07ec40fad
陶红兵 <mary3@ualberta.ca>
2018-12-06 18:38:30 -0500

    test

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -1,3 +1,4 @@
+import java.util.PriorityQueue;
 
 /**
  * Although this class has a history of several years,
@@ -42,13 +43,85 @@ public class HuffProcessor {
 	 */
 	public void compress(BitInputStream in, BitOutputStream out){
 
-		while (true){
-			int val = in.readBits(BITS_PER_WORD);
-			if (val == -1) break;
-			out.writeBits(BITS_PER_WORD, val);
-		}
+		int[] counts = readForCounts(in);
+		HuffNode root = makeTreeFromCounts(counts);
+		String[] codings = makeCodingsFromTree(root);
+		
+		out.writeBits(BITS_PER_INT, HUFF_TREE);
+		writeHeader(root, out);
+		
+		in.reset();
+		writeCompressedBits(codings, in , out);
 		out.close();
 	}
+	private void writeHeader(HuffNode root, BitOutputStream out) {
+		if (root.myLeft == null || root.myRight == null){
+			out.writeBits(1, 1);
+			out.writeBits(BITS_PER_WORD + 1, root.myValue);
+		}
+		else {
+			out.writeBits(1, 0);
+		}
+		
+		writeHeader(root.myLeft, out);
+		writeHeader(root.myRight, out);
+		
+		
+	}
+
+	private String[] makeCodingsFromTree(HuffNode root) {
+		String[] encodings = new String[ALPH_SIZE + 1];
+		codingHelper(root, "", encodings);
+		
+		return encodings;
+	}
+
+	
+	private String path = "";
+	private void codingHelper(HuffNode root, String s, String[] encodings) {
+		path = path + s;
+		if (root.myLeft == null && root.myRight == null){
+			encodings[root.myValue] = path;
+			return;
+		}
+		
+		codingHelper(root.myLeft, "0", encodings);
+		codingHelper(root.myRight, "1", encodings);
+	}
+
+	private HuffNode makeTreeFromCounts(int[] counts) {
+		PriorityQueue<HuffNode> pq = new PriorityQueue<>();
+		for (int i = 0; i < counts.length; i++){
+			if (counts[i] > 0) {
+				pq.add(new HuffNode(i, counts[i], null, null));
+			}
+		}
+		
+		while (pq.size() > 1){
+			HuffNode left = pq.remove();
+			HuffNode right = pq.remove();
+			int weight = left.myWeight + right.myWeight; //account for subtrees too
+			HuffNode t = new HuffNode(0, weight, null, null);
+			pq.add(t);
+		}
+		
+		HuffNode root = pq.remove();
+		return null;
+	}
+
+	private int[] readForCounts(BitInputStream in) {
+		int[] freq = new int[ALPH_SIZE + 1];
+		
+		int bit = in.readBits(BITS_PER_WORD);
+		while(bit != -1){
+			freq[bit]+=1;
+			bit = in.readBits(BITS_PER_WORD);
+		}
+		freq[PSEUDO_EOF] = 1;
+		
+		return freq;
+	}
+
 	/**
 	 * Decompresses a file. Output file must be identical bit-by-bit to the
 	 * original.
@@ -59,12 +132,57 @@ public class HuffProcessor {
 	 *            Buffered bit stream writing to the output file.
 	 */
 	public void decompress(BitInputStream in, BitOutputStream out){
-
-		while (true){
-			int val = in.readBits(BITS_PER_WORD);
-			if (val == -1) break;
-			out.writeBits(BITS_PER_WORD, val);
+		
+		int bits = in.readBits(BITS_PER_INT);
+		if (bits != HUFF_TREE){
+			throw new HuffException("illegal header starts with "+bits);
 		}
+		
+		HuffNode root = readTreeHeader(in);
+		readCompressedBits(root, in, out);
+
+		
 		out.close();
+	}
+
+	private void readCompressedBits(HuffNode root, BitInputStream in, BitOutputStream out) {
+		HuffNode current = root;
+		while (true) {
+			int bits = in.readBits(1);
+			if (bits == -1){
+				throw new HuffException("bad input, no PSEUDO_EOF");
+			}
+			else {
+				if (bits == 0) current = current.myLeft;
+				else current = current.myRight;
+				
+				if (current.myLeft == null && current.myRight == null){
+					if (current.myValue == PSEUDO_EOF) break;
+					else {
+						out.writeBits(1, current.myValue);
+						current = root;
+					}
+					
+				}
+			}
+		}
+		
+	}
+
+	private HuffNode readTreeHeader(BitInputStream in) {
+		int bit = in.readBits(BITS_PER_WORD + 1);
+		if (bit == -1){
+			throw new HuffException("illegal header starts with "+bit);
+		}
+		
+		if (bit == 0){
+			HuffNode left = readTreeHeader(in);
+			HuffNode right = readTreeHeader(in);
+			return new HuffNode(0, 0, left, right);
+		}
+		else {
+			int value = in.readBits(9);
+			return new HuffNode(value, 0, null, null);
+		}
 	}
 }
